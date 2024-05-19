79d3605fb8631a3a0578a5ecdb53705f14855236
Pasquale.Silv <55320885+Pasquale-Silv@users.noreply.github.com>
2024-05-20 00:31:15 +0200

    SC tested

--- projects/voting-contracts/smart_contracts/voting_sc/contract.py
+++ projects/voting-contracts/smart_contracts/voting_sc/contract.py
@@ -2,6 +2,7 @@ from algopy import ARC4Contract, arc4
 
 
 class VotingSc(ARC4Contract):
+    
     @arc4.abimethod()
     def hello(self, name: arc4.String) -> arc4.String:
         return "Hello, " + name
